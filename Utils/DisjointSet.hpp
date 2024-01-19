b11ed4d48e9d320533a13f2f3bb7298919e6a512
Aledo-Jun <june050190@gmail.com>
2024-01-19 19:07:58 +0900

    wrap up into namespace Utils

--- Utils/DisjointSet.hpp
+++ Utils/DisjointSet.hpp
@@ -1,44 +1,50 @@
 #include <vector>
 
 using namespace std;
-
-class disjointSet {
-    const int def_sz = 1001;
-    vector<int> parent;
-    vector<int> rank;
-public:
-    disjointSet(){
-        parent.assign(def_sz, 0);
-        rank.assign(def_sz, 0);
-    }
-    disjointSet(int sz){
-        parent.assign(sz, 0);
-        for (int i = 0; i < sz; i++)
-            parent[i] = i;
-        rank.assign(sz, 0);
-    }
-
-    // find the root of x
-    int find(int x){
-        if (x != parent[x]){
-            parent[x] = find(parent[x]);
+namespace Utils
+{
+namespace DisjointSet
+{
+    class disjointSet {
+        const int def_sz = 1001;
+        vector<int> parent;
+        vector<int> rank;
+    public:
+        disjointSet() {
+            parent.assign(def_sz, 0);
+            rank.assign(def_sz, 0);
         }
-        return parent[x];
-    }
 
-    void uni(int x, int y){
-        int rootx = find(x);
-        int rooty = find(y);
+        disjointSet(int sz) {
+            parent.assign(sz, 0);
+            for (int i = 0; i < sz; i++)
+                parent[i] = i;
+            rank.assign(sz, 0);
+        }
 
-        if (rootx != rooty){
-            if (rank[rootx] < rank[rooty])
-                parent[rootx] = rooty;
-            else if (rank[rootx] > rank[rooty])
-                parent[rooty] = rootx;
-            else {
-                parent[rooty] = rootx;
-                rank[rootx]++;
+        // find the root of x
+        int find(int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        void uni(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+
+            if (rootx != rooty) {
+                if (rank[rootx] < rank[rooty])
+                    parent[rootx] = rooty;
+                else if (rank[rootx] > rank[rooty])
+                    parent[rooty] = rootx;
+                else {
+                    parent[rooty] = rootx;
+                    rank[rootx]++;
+                }
             }
         }
-    }
-};
+    }; // class disjointSet
+} // namespace DisjointSet
+} // namespace Utils
