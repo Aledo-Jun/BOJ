afd27e8638da0c97a647bc8bf064738ca9fd79d6
Aledo-Jun <june050190@gmail.com>
2024-02-17 23:48:47 +0900

    Add 'supports_arithmetic' and alias using

--- Utils/TypeTraits.hpp
+++ Utils/TypeTraits.hpp
@@ -25,5 +25,22 @@ namespace Utils::TypeTraits
 
     template<typename T>
     using is_unsigned_int_t = std::enable_if_t<is_unsigned_int<T>::value>;
+
+    template<typename...> struct to_void { using type = void; };
+    template<typename... Ts> using void_t = typename to_void<Ts...>::type;
+
+    template<typename T, typename = void>
+    struct supports_arithmetic : public std::false_type { };
+
+    template<typename T>
+    struct supports_arithmetic<T,
+            void_t<decltype(std::declval<T>() + std::declval<T>()),
+                   decltype(std::declval<T>() - std::declval<T>()),
+                   decltype(std::declval<T>() * std::declval<T>()),
+                   decltype(std::declval<T>() / std::declval<T>())>>
+    : public std::true_type { };
+
+    template<typename T>
+    using supports_arithmetic_v = typename supports_arithmetic<T>::value;
 }
 #endif //BOJ_TYPETRAITS_HPP
