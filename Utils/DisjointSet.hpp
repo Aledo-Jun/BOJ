a139fa3c219172815a9e4ac8c845c847a8625b14
Aledo-Jun <june050190@gmail.com>
2024-02-28 21:50:04 +0900

    Add Weighted D.S.

--- Utils/DisjointSet.hpp
+++ Utils/DisjointSet.hpp
@@ -72,5 +72,53 @@ namespace DisjointSet
                 parent[rooty] = rootx;
         }
     }; // class DisjointSet
+
+    class WeightedDisjointSet {
+    private:
+        const int def_sz = 1001;
+        vector<pair<int,int>> parent;
+        vector<int> rank;
+    public:
+        WeightedDisjointSet() {
+            parent.assign(def_sz, {0,0});
+            rank.assign(def_sz, 0);
+        }
+
+        explicit WeightedDisjointSet(int sz) {
+            parent.assign(sz, {0,0});
+            for (int i = 0; i < sz; i++)
+                parent[i] = {i, 0};
+            rank.assign(sz, 0);
+        }
+
+        // find the root of x
+        pair<int,int> find(int x, int w) {
+            if (x != parent[x].first) {
+                auto [p, pw] = find(parent[x].first, parent[x].second + w);
+                parent[x] = {p, pw - w};
+            }
+            return {parent[x].first, parent[x].second + w};
+        }
+
+        // Union By Rank,
+        // i.e. the parent relation could be relational(not specified).
+        void uni(int x, int y, int w) {
+            auto [rootx, wx] = find(x, 0);
+            auto [rooty, wy] = find(y, 0);
+
+            if (rootx != rooty) {
+                if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = {rooty, wy - wx - w};
+                }
+                else if (rank[rootx] > rank[rooty])
+                    parent[rooty] = {rootx, wx - wy + w};
+                else {
+                    parent[rooty] = {rootx, wx - wy + w};
+                    rank[rootx]++;
+                }
+            }
+        }
+    }; // class WeightedDisjointSet
+
 } // namespace DisjointSet
 } // namespace Utils
