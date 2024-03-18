9bbf27447556cb964632c648a45d1adb5490416c
Aledo-Jun <june050190@gmail.com>
2024-03-18 22:13:47 +0900

    minor change

--- Utils/Graph.hpp
+++ Utils/Graph.hpp
@@ -469,7 +469,6 @@ namespace Graph {
                     }
                 }
             }
-
             return 0;
         }
 
@@ -493,71 +492,77 @@ namespace Graph {
         }
     };
 
-        class MinCostMaxFlow {
-            const int INF = 1e9;
-            struct Edge {
-                int u, v, cap, rev, cost;
-                Edge(int u, int v, int cap, int rev, int cost) : u(u), v(v), cap(cap), rev(rev), cost(cost) {}
-            };
-        private:
-            int n;
-            vector<vector<Edge>> g;
-            vector<int> dist;
-            vector<Edge*> prev;
-            bool bellmanFord(int s, int t) {
-                dist.assign(n, INF);
-                queue<int> q;
-                vector<bool> is_in_q(n, false);
+    class MinCostMaxFlow {
+        const int INF = 1e9;
 
-                dist[s] = 0;
-                q.emplace(s);
-                is_in_q[s] = true;
+        struct Edge {
+            int u, v, cap, rev, cost;
 
-                while (!q.empty()) {
-                    int u = q.front();
-                    q.pop();
-                    is_in_q[u] = false;
-                    for (auto &e: g[u]) {
-                        if (e.cap > 0 && dist[e.v] > dist[u] + e.cost) {
-                            dist[e.v] = dist[u] + e.cost;
-                            prev[e.v] = &e;
-                            if (!is_in_q[e.v]) {
-                                is_in_q[e.v] = true;
-                                q.emplace(e.v);
-                            }
+            Edge(int u, int v, int cap, int rev, int cost) : u(u), v(v), cap(cap), rev(rev), cost(cost) {}
+        };
+
+    private:
+        int n;
+        vector<vector<Edge>> g;
+        vector<int> dist;
+        vector<Edge *> prev;
+
+        bool bellmanFord(int s, int t) {
+            dist.assign(n, INF);
+            queue<int> q;
+            vector<bool> is_in_q(n, false);
+
+            dist[s] = 0;
+            q.emplace(s);
+            is_in_q[s] = true;
+
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                is_in_q[u] = false;
+                for (auto &e: g[u]) {
+                    if (e.cap > 0 && dist[e.v] > dist[u] + e.cost) {
+                        dist[e.v] = dist[u] + e.cost;
+                        prev[e.v] = &e;
+                        if (!is_in_q[e.v]) {
+                            is_in_q[e.v] = true;
+                            q.emplace(e.v);
                         }
                     }
                 }
-                return dist[t] != INF;
             }
+            return dist[t] != INF;
+        }
 
-        public:
-            explicit MinCostMaxFlow(int n) : n(n), g(n), dist(n), prev(n) {}
-            void addEdge(int u, int v, int cap, int cost) {
-                g[u].emplace_back(u, v, cap, g[v].size(), cost);
-                g[v].emplace_back(v, u, 0, g[u].size() - 1, -cost);
-            }
-            pair<int, int> minCostMaxFlow(int s, int t) {
-                int flow = 0, cost = 0;
-                while (bellmanFord(s, t)) {
-                    // find minimum flow
-                    int curr = INF;
-                    for (auto e = prev[t]; e; e = prev[e->u]){
-                        curr = min(curr, e->cap);
-                    }
-                    flow += curr;
+    public:
+        explicit MinCostMaxFlow(int n) : n(n), g(n), dist(n), prev(n) {}
 
-                    for (auto e = prev[t]; e; e = prev[e->u]){
-                        cost += curr * e->cost;
+        void addEdge(int u, int v, int cap, int cost) {
+            g[u].emplace_back(u, v, cap, g[v].size(), cost);
+            g[v].emplace_back(v, u, 0, g[u].size() - 1, -cost);
+        }
 
-                        // reverse direction
-                        e->cap -= curr;
-                        g[e->v][e->rev].cap += curr;
-                    }
+        pair<int, int> minCostMaxFlow(int s, int t) {
+            int flow = 0, cost = 0;
+            while (bellmanFord(s, t)) {
+                // find minimum flow
+                int curr = INF;
+                for (auto e = prev[t]; e; e = prev[e->u]) {
+                    curr = min(curr, e->cap);
                 }
-                return {flow, cost};
+                flow += curr;
+
+                for (auto e = prev[t]; e; e = prev[e->u]) {
+                    cost += curr * e->cost;
+
+                    // reverse direction
+                    e->cap -= curr;
+                    g[e->v][e->rev].cap += curr;
+                }
             }
-        };
+            return {flow, cost};
+        }
+    };
 
 #pragma ide diagnostic pop
 } // namespace Graph
