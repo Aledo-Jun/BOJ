532fb3feff6d6d6dba87f913b632d8c27f91f392
Aledo-Jun <june050190@gmail.com>
2024-03-20 21:04:34 +0900

    Add Aho-Corasick

--- Utils/String.hpp
+++ Utils/String.hpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <vector>
+#include <memory>
+#include <queue>
 using ull = unsigned long long;
 
 using namespace std;
@@ -57,6 +59,90 @@ namespace String
         }
     };
 
+ /**
+ * Aho-Corasick algorithm for string-searching in linear time
+ * @tparam size size of the input character set(default is english alphabet, 26)
+ * @tparam base the starting character of the set above(default is 'a')
+ */
+    template<int size = 26, char base = 'a'>
+    class Aho_Corasick {
+    private:
+        struct TrieNode {
+            std::unique_ptr<TrieNode> children[size]{};
+            TrieNode* fail;
+            bool isEnd;
+
+            TrieNode() : isEnd(false), fail(nullptr) {
+                fill(children, children + size, nullptr);
+            }
+
+            void insert(const string& word) {
+                TrieNode* current = this;
+                for (char c: word) {
+                    int index = c - base;
+                    if (current->children[index] == nullptr)
+                        current->children[index] = make_unique<TrieNode>();
+
+                    current = current->children[index].get();
+                }
+                current->isEnd = true;
+            }
+        };
+
+        TrieNode* root;
+        bool is_initialized = false;
+
+    public:
+        void init() {
+            queue<TrieNode*> q;
+            q.emplace(root);
+            root->fail = root;
+
+            while (!q.empty()) {
+                auto current = q.front();
+                q.pop();
+                for (int i = 0; i < size; i++) {
+                    auto next = current->children[i].get();
+                    if (next == nullptr) continue;
+
+                    if (current == root) next->fail = root;
+                    else {
+                        auto fail_node = current->fail;
+                        while (fail_node != root && fail_node->children[i] == nullptr)
+                            fail_node = fail_node->fail;
+                        if (fail_node->children[i] != nullptr) fail_node = fail_node->children[i].get();
+
+                        next->fail = fail_node;
+                    }
+                    if (next->fail->isEnd) next->isEnd = true;
+
+                    q.emplace(next);
+                }
+            }
+        }
+
+        void insert(const string& word) {
+            root->insert(word);
+        }
+
+        Aho_Corasick() : root(new TrieNode) {}
+
+        bool find(const string& word) {
+            if (!is_initialized) {
+                init();
+                is_initialized = true;
+            }
+            auto current = root;
+            for (const auto& c: word) {
+                int index = c - base;
+                while (current != root && current->children[index] == nullptr)
+                    current = current->fail;
+                if (current->children[index] != nullptr) current = current->children[index].get();
+                if (current->isEnd) return true;
+            }
+            return false;
+        }
+    };
 
     // Find pattern's partial matches(failure table) for KMP algorithm
     vector<int> getPartialMatch(const string &P) {
