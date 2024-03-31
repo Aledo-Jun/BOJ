5075326efed12458d74bf78eea92fa3666080072
Aledo-Jun <june050190@gmail.com>
2024-04-01 00:20:44 +0900

    Add SA-IS

--- Utils/String.hpp
+++ Utils/String.hpp
@@ -3,6 +3,8 @@
 #include <memory>
 #include <queue>
 #include <functional>
+#include <cassert>
+#include <limits>
 using ull = unsigned long long;
 
 using namespace std;
@@ -211,6 +213,8 @@ namespace String
         std::vector<int> cnt, aux; // for counting sort
         std::vector<int> sfx, rev, lcp;
 
+        bool lcp_initialized = false;
+
     public:
         explicit SuffixArray(const std::string& s) : str(s), n((int)s.size()),
                                                      ord(n + 1), nord(n + 1),
@@ -255,10 +259,10 @@ namespace String
                 p *= 2;
             }
             for (int i = 0; i < n; i++) rev[sfx[i]] = i;
+        }
 
-            // LCP
-            int h = 0;
-            for (int i = 0; i < n; i++) {
+        void build_lcp() {
+            for (int i = 0, h = 0; i < n; i++) {
                 if (rev[i]) {
                     int prev = sfx[rev[i] - 1];
                     while (str[prev + h] == str[i + h]) h++;
@@ -269,7 +273,178 @@ namespace String
         }
 
         int operator[](int i) { return sfx[i]; }
-        std::vector<int> get_lcp() { return lcp; }
+        std::vector<int> get_lcp() {
+            if (lcp_initialized) return lcp;
+
+            build_lcp();
+            return lcp;
+        }
+    };
+
+    template<typename String = std::string>
+    class SA_IS {
+        using T = typename String::value_type;
+    private:
+        int sz;
+        std::vector<int> str; // compressed string
+        int bucket_sz; // bucket size after compression (== # of distinct characters in the string)
+
+        std::vector<int> sfx, rev, lcp;
+        std::vector<int> ptr; // for counting sort
+
+        bool sfx_initialized = false, lcp_initialized = false;
+
+        void build_sfx(const std::vector<int>& s, int bucket, std::vector<int>& sa) {
+            int n = static_cast<int>(s.size());
+            if (n == 1) {
+                sa[0] = 0;
+                return;
+            }
+
+            std::vector<bool> is_S_type(n, false);
+            std::vector<int> cnt(bucket + 1, 0); // for counting sort
+
+            // 1. Check the type of each character and the number of LMS.
+            int lms_cnt = 0;
+            for (int i = n - 2; i >= 0; i--) {
+                if (s[i] < s[i + 1]) is_S_type[i] = true;
+                else if (s[i] == s[i + 1]) is_S_type[i] = is_S_type[i + 1];
+                else lms_cnt += is_S_type[i + 1];
+            }
+
+            std::fill(sa.begin(), sa.end(), -1);
+            for (int i = 0; i < n; i++) cnt[s[i] + 1]++;
+            for (int i = 1; i <= bucket; i++) cnt[i] += cnt[i - 1];
+
+            // 2. Sort LMS's using counting sort.
+            if (lms_cnt) {
+                std::vector<int> lms_pos(lms_cnt + 1), sub(lms_cnt, 0);
+                std::vector<int> sub_sa(lms_cnt, 0); // Guessed SA only using LMS's
+
+                // 2-1. Specify the position of LMS's
+                ptr.assign(std::next(cnt.begin()), cnt.end());
+                for (int i = 1, j = 0; i < n; i++) {
+                    if (is_S_type[i] && !is_S_type[i - 1]) {
+                        rev[i] = j; // there is j-th lms at i
+                        lms_pos[j++] = i;
+                        sa[--ptr[s[i]]] = i;
+                    }
+                }
+                lms_pos[lms_cnt] = n;
+
+                // 2-2. Induced sorting.
+                ptr.assign(cnt.begin(), std::prev(cnt.end()));
+                sa[ptr[s[n - 1]]++] = n - 1;
+                for (int i = 0; i < n; i++) { // sort L-type (left->right)
+                    int j = sa[i] - 1;
+                    if (j >= 0 && !is_S_type[j])
+                        sa[ptr[s[j]]++] = j;
+                }
+                ptr.assign(std::next(cnt.begin()), cnt.end());
+                for (int i = n - 1; i >= 0; i--) { // sort S-type (right->left)
+                    int j = sa[i] - 1;
+                    if (j >= 0 && is_S_type[j])
+                        sa[--ptr[s[j]]] = j;
+                }
+
+                for (int i = 0, j = 0; i < n; i++) {
+                    if (sa[i] > 0 && is_S_type[sa[i]] && !is_S_type[sa[i] - 1]) {
+                        sub_sa[j++] = rev[sa[i]];
+                    }
+                }
+
+                // 2-3. For each consecutive LMS's in sub_sa,
+                for (int i = 1; i < lms_cnt; i++) {
+                    int q1 = sub_sa[i - 1], q2 = sub_sa[i];
+                    int j1 = lms_pos[q1], j2 = lms_pos[q2];
+                    int n1 = lms_pos[q1 + 1] - j1;
+                    int n2 = lms_pos[q2 + 1] - j2;
+                    sub[q2] = sub[q1];
+                    // Check whether they are equal or not.
+                    // If not, give the second one another name.
+                    if (n1 != n2) sub[q2]++;
+                    else {
+                        for (int j = 0; j < n1; j++) {
+                            if (s[j1 + j] < s[j2 + j] || is_S_type[j1 + j] < is_S_type[j2 + j]) {
+                                sub[q2]++;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // 2-4. At this point, sub_sa is sorted.
+                // But the actual SA might not be sorted properly(might exist duplicates).
+                int sub_bucket = sub[sub_sa[lms_cnt - 1]] + 1;
+                if (sub_bucket < lms_cnt)               // if not properly sorted,
+                    build_sfx(sub, sub_bucket, sub_sa); // recurse
+
+                // 2-5. Merge sorted sub_sa to sa.
+                std::fill(sa.begin(), sa.end(), -1);
+                ptr.assign(std::next(cnt.begin()), cnt.end());
+                for (int i = lms_cnt - 1; i >= 0; i--) {
+                    int j = lms_pos[sub_sa[i]];
+                    sa[--ptr[s[j]]] = j;
+                }
+            }
+
+            // 3. Finally, clean up the rest of the characters.
+            ptr.assign(cnt.begin(), std::prev(cnt.end()));
+            sa[ptr[s[n - 1]]++] = n - 1;
+            for (int i = 0; i < n; i++) {
+                int j = sa[i] - 1;
+                if (j >= 0 && !is_S_type[j])
+                    sa[ptr[s[j]]++] = j;
+            }
+            ptr.assign(std::next(cnt.begin()), cnt.end());
+            for (int i = n - 1; i >= 0; i--) {
+                int j = sa[i] - 1;
+                if (j >= 0 && is_S_type[j])
+                    sa[--ptr[s[j]]] = j;
+            }
+        }
+
+        void build_lcp() {
+            assert(sfx_initialized);
+            for (int i = 0; i < sz; i++) rev[sfx[i]] = i;
+            str.emplace_back(-1); // IDK why this is needed. maybe sentinel or something?
+            for (int i = 0, h = 0; i < sz; i++) {
+                if (rev[i]) {
+                    int prev = sfx[rev[i] - 1];
+                    while (str[prev + h] == str[i + h]) h++;
+                    lcp[rev[i]] = h;
+                }
+                h = std::max(h - 1, 0);
+            }
+            str.pop_back();
+        }
+
+    public:
+        /* implicit */ SA_IS(const String& s) // NOLINT
+                : sz(static_cast<int>(s.size())), str(sz), rev(sz), sfx(sz)
+        {
+            const auto mn = std::numeric_limits<T>::min();
+            const auto mx = std::numeric_limits<T>::max();
+            std::vector<int> compressor(mx - mn + 1);
+            for (int i = 0; i < sz; i++) compressor[s[i] - mn] = 1;
+            for (int i = 1; i < static_cast<int>(compressor.size()); i++) compressor[i] += compressor[i - 1];
+            for (int i = 0; i < sz; i++) str[i] = compressor[s[i] - mn] - 1;
+            bucket_sz = compressor.back();
+
+            build_sfx(str, bucket_sz, sfx);
+            sfx_initialized = true;
+        }
+
+        int operator[](int i) const { return sfx[i]; }
+        std::vector<int> get_sfx() { return sfx; }
+        std::vector<int> get_lcp() {
+            if (lcp_initialized) return lcp;
+
+            lcp.resize(sz, 0);
+            build_lcp();
+            lcp_initialized = true;
+            return lcp;
+        }
     };
 } // namespace String
 } // namespace Utils
