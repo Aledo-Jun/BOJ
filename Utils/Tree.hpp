8a555162a9cd66f2bf62c5a3844c27b0477d7c56
Aledo-Jun <june050190@gmail.com>
2024-04-28 21:16:13 +0900

    Add SplayTree

--- Utils/Tree.hpp
+++ Utils/Tree.hpp
@@ -93,6 +93,236 @@ namespace Tree
         }
     };
 
+
+    /**
+     * Splay tree implementation
+     * @tparam T Value type
+     */
+    template<typename T>
+    class SplayTree {
+        struct Node {
+            Node *l, *r, *p;
+            T key; // value stored in the node
+            int cnt; // # of nodes in the subtree
+
+            /* Add more variables if needed */
+
+            Node() : l(nullptr), r(nullptr), p(nullptr), key(T(0)), cnt(1) {}
+            Node(T key) : l(nullptr), r(nullptr), p(nullptr), key(key), cnt(1) {
+                /* Initialize extra variables */
+            }
+        } *tree;
+
+        // modify if needed
+        void update(Node *x) {
+            x->cnt = 1;
+            if (x->l) x->cnt += x->l->cnt;
+            if (x->r) x->cnt += x->r->cnt;
+        }
+
+        void rotate(Node *x) {
+            Node *p = x->p;
+            Node *b;
+            if (x == p->l) {
+                p->l = b = x->r;
+                x->r = p;
+            } else {
+                p->r = b = x->l;
+                x->l = p;
+            }
+            x->p = p->p;
+            p->p = x;
+            if (b) b->p = p;
+            (x->p ? p == x->p->l ? x->p->l : x->p->r : tree) = x;
+
+            update(p);
+            update(x);
+        }
+
+        void splay(Node *x) {
+            while (x->p) {
+                Node *p = x->p;
+                Node *g = p->p;
+                if (g) rotate((x == p->l) == (p == g->l) ? p : x);
+                rotate(x);
+            }
+        }
+
+    public:
+        explicit SplayTree(const std::vector<T> &v) {
+            Node *p = tree = new Node(0);
+            for (const auto& e : v) {
+                p->r = new Node(e);
+                p->r->p = p;
+                p = p->r;
+            }
+            p->r = new Node(0);
+            p->r->p = p;
+            p = p->r;
+
+            while (p) {
+                update(p);
+                p = p->p;
+            }
+        }
+
+        /**
+         * Insert a new node with the given key
+         * @param key the key value to insert
+         */
+        void insert(T key) {
+            Node *p = tree, **pp;
+            if (!p) {
+                Node *x = new Node(key);
+                tree = x;
+                return;
+            }
+            while (true) {
+                if (key == p->key) return; // no duplicate allowed
+                if (key < p->key) {
+                    if (!p->l) {
+                        pp = &p->l;
+                        break;
+                    }
+                    p = p->l;
+                } else {
+                    if (!p->r) {
+                        pp = &p->r;
+                        break;
+                    }
+                    p = p->r;
+                }
+            }
+            Node *x = new Node(key);
+            *pp = x;  // p -> x
+            x->p = p; // x -> p
+
+            splay(x); // Crucial!
+        }
+
+        /**
+         * The node with the given key becomes the root node after the function call
+         * @param key the key value to find
+         * @return true if found, false otherwise
+         */
+        bool find(T key) {
+            Node *p = tree;
+            if (!p) return false;
+            while (p) {
+                if (key == p->key) break;
+                if (key < p->key) {
+                    if (!p->l) break;
+                    p = p->l;
+                } else {
+                    if (!p->r) break;
+                    p = p->r;
+                }
+            }
+            splay(p);
+            return key == p->key;
+        }
+
+        /**
+         * Erase the node with the given key
+         */
+        void erase(T key) {
+            if (!find(key)) return;
+            Node *p = tree;
+            if (p->l) {
+                if (p->r) {
+                    tree = p->l;
+                    tree->p = nullptr;
+
+                    Node *x = tree;
+                    while (x->r) x = x->r;
+                    x->r = p->r;
+                    p->r->p = x;
+                    splay(x);
+                    delete p;
+                    return;
+                }
+                tree = p->l;
+                tree->p = nullptr;
+                delete p;
+                return;
+            }
+            if (p->r) {
+                tree = p->r;
+                tree->p = nullptr;
+                delete p;
+                return;
+            }
+            delete p;
+            tree = nullptr;
+        }
+
+        /**
+         * Make the root node to be the k-th
+         */
+        void find_kth(int k) {
+            Node *x = tree;
+            while (true) {
+                while (x->l && x->l->cnt > k) x = x->l;
+                if (x->l) k -= x->l->cnt;
+                if (!k--) break;
+                x = x->r;
+            }
+            splay(x);
+        }
+
+        /**
+         * Make the tree->r->l to represent the given interval [l,r]
+         */
+        void interval(int l, int r) {
+            find_kth(l - 1);
+            auto *p = tree;
+            tree = p->r;
+            tree->p = nullptr;
+            find_kth(r - l + 1);
+            p->r = tree;
+            tree->p = p;
+            tree = p;
+        }
+
+        /**
+         * Insert a new node with the key of val into the given pos
+         */
+        void insert(int pos, T val) {
+            find_kth(pos);
+            auto *p = tree, *x = new Node(val);
+            x->p = p;
+            p->l->p = x;
+            x->l = p->l;
+            p->l = x;
+            splay(x);
+        }
+
+        /**
+         * Erase the node at the given pos
+         */
+        void erase_at(int pos) {
+            interval(pos, pos);
+            auto *x = tree->r->l;
+            x->p->l = nullptr;
+            splay(x->p);
+            delete x;
+        }
+
+        /**
+         * Update the node's key at pos to val
+         */
+        void update(int pos, T val) {
+            find_kth(pos);
+            tree->key = val;
+            update(tree);
+        }
+
+        T query(int l, int r, int k) {
+            interval(l, r);
+            // Do something within the interval
+        }
+    };
+
 } // namespace Tree
 } // namespace Utils
 
