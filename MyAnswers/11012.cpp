abf3669c46272edb739c0aecc62704a33f63ca77
Aledo-Jun <june050190@gmail.com>
2024-05-06 21:19:49 +0900

    solve 11012

---
+++ MyAnswers/11012.cpp
@@ -0,0 +1,478 @@
+//
+// Created by june0 on 2024-05-06.
+//
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+//#pragma GCC optimize("O3,unroll-loops")
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define all(v) (v).begin(), (v).end()
+using namespace std;
+using namespace __gnu_cxx;
+using ll [[maybe_unused]] = long long;
+using ull [[maybe_unused]] = unsigned long long;
+using lll [[maybe_unused]] = __int128;
+using lld [[maybe_unused]] = long double;
+template<typename T> using graph [[maybe_unused]] = vector<vector<pair<int,T>>>;
+template<typename T> using matrix [[maybe_unused]] = vector<vector<T>>;
+
+/**
+ *  "Premature optimization is the root of all evil."
+ *  <sub> Donald Knuth </sub>
+ */
+
+#ifdef ONLINE_JUDGE // Be careful if problem is about strings.
+/**
+ * Namespace for Fast I/O
+ *
+ * @class@b INPUT
+ * class which can replace the cin
+ *
+ * @class@b OUTPUT
+ * class which can replace the cout
+ *
+ * @Description
+ * These classes use low-level i/o functions (@c fread() for input, @c fwrite() for output)
+ * so that they can read or write much faster than cin and cout. <br>
+ * Several macros are defined for convenience of using them.
+ *
+ * @Author  <a href="https://blog.naver.com/jinhan814/222266396476">jinhan814</a>
+ * @Date    2021-05-06
+ */
+namespace FastIO {
+    constexpr int SZ = 1 << 20;
+
+    /* Class INPUT */
+    class INPUT {
+    private:
+        char readBuffer[SZ];
+        int read_idx, next_idx;
+        bool __END_FLAG__, __GET_LINE_FLAG__; // NOLINT
+    public:
+        explicit operator bool() const { return !__END_FLAG__; }
+
+        static bool IsBlank(char c) { return c == ' ' || c == '\n'; }
+
+        static bool IsEnd(char c) { return c == '\0'; }
+
+        char _ReadChar() { // NOLINT
+            if (read_idx == next_idx) {
+                next_idx = (int)fread(readBuffer, sizeof(char), SZ, stdin);
+                if (next_idx == 0) return 0;
+                read_idx = 0;
+            }
+            return readBuffer[read_idx++];
+        }
+
+        char ReadChar() {
+            char ret = _ReadChar();
+            for (; IsBlank(ret); ret = _ReadChar());
+
+            return ret;
+        }
+
+        template<class T>
+        T ReadInt() {
+            T ret = 0;
+            char curr = _ReadChar();
+            bool minus_flag = false;
+
+            for (; IsBlank(curr); curr = _ReadChar());
+            if (curr == '-') minus_flag = true, curr = _ReadChar();
+            for (; !IsBlank(curr) && !IsEnd(curr); curr = _ReadChar())
+                ret = 10 * ret + (curr & 15);
+            if (IsEnd(curr)) __END_FLAG__ = true;
+
+            return minus_flag ? -ret : ret;
+        }
+
+        std::string ReadString() {
+            std::string ret;
+            char curr = _ReadChar();
+            for (; IsBlank(curr); curr = _ReadChar());
+            for (; !IsBlank(curr) && !IsEnd(curr); curr = _ReadChar())
+                ret += curr;
+            if (IsEnd(curr)) __END_FLAG__ = true;
+
+            return ret;
+        }
+
+        double ReadDouble() {
+            std::string ret = ReadString();
+            return std::stod(ret);
+        }
+
+        std::string getline() {
+            std::string ret;
+            char curr = _ReadChar();
+            for (; curr != '\n' && !IsEnd(curr); curr = _ReadChar())
+                ret += curr;
+            if (__GET_LINE_FLAG__) __END_FLAG__ = true;
+            if (IsEnd(curr)) __GET_LINE_FLAG__ = true;
+
+            return ret;
+        }
+
+        friend INPUT &getline(INPUT &in, std::string &s) {
+            s = in.getline();
+            return in;
+        }
+    } _in;
+    /* End of Class INPUT */
+
+    /* Class OUTPUT */
+    class OUTPUT {
+    private:
+        char writeBuffer[SZ];
+        int write_idx;
+    public:
+        ~OUTPUT() { Flush(); }
+
+        explicit operator bool() const { return true; }
+
+        void Flush() {
+            fwrite(writeBuffer, sizeof(char), write_idx, stdout);
+            write_idx = 0;
+        }
+
+        void WriteChar(char c) {
+            if (write_idx == SZ) Flush();
+            writeBuffer[write_idx++] = c;
+        }
+
+        template<class T>
+        void WriteInt(T total) {
+            int sz = GetSize(total);
+            if (write_idx + sz + 1 >= SZ) Flush();
+            if (total < 0) writeBuffer[write_idx++] = '-', total = -total;
+            for (int i = sz; i-- > 0; total /= 10)
+                writeBuffer[write_idx + i] = total % 10 | 48;
+            write_idx += sz;
+        }
+
+        void WriteString(const std::string& s) {
+            for (auto &c: s) WriteChar(c);
+        }
+
+        void WriteDouble(double d) {
+            WriteString(std::to_string(d));
+        }
+
+        template<class T>
+        int GetSize(T total) {
+            int ret = 1;
+            for (total = total >= 0 ? total : -total; total >= 10; total /= 10) ret++;
+
+            return ret;
+        }
+    } _out;
+    /* End of Class OUTPUT */
+
+    /* Operators */
+    INPUT &operator>>(INPUT &in, char &i) {
+        i = in.ReadChar();
+        return in;
+    }
+
+    INPUT &operator>>(INPUT &in, std::string &i) {
+        i = in.ReadString();
+        return in;
+    }
+
+    template<class T, typename std::enable_if_t<std::is_arithmetic_v<T>> * = nullptr>
+    INPUT &operator>>(INPUT &in, T &i) {
+        if constexpr (std::is_floating_point_v<T>) i = in.ReadDouble();
+        else if constexpr (std::is_integral_v<T>) i = in.ReadInt<T>();
+        return in;
+    }
+
+    OUTPUT &operator<<(OUTPUT &out, char i) {
+        out.WriteChar(i);
+        return out;
+    }
+
+    OUTPUT &operator<<(OUTPUT &out, const std::string &i) {
+        out.WriteString(i);
+        return out;
+    }
+
+    template<class T, typename std::enable_if_t<std::is_arithmetic_v<T>> * = nullptr>
+    OUTPUT &operator<<(OUTPUT &out, T i) {
+        if constexpr (std::is_floating_point_v<T>) out.WriteDouble(i);
+        else if constexpr (std::is_integral_v<T>) out.WriteInt(i);
+        return out;
+    }
+
+    /* Macros for convenience */
+    #undef fastIO
+    #define fastIO 1
+    #define cin _in
+    #define cout _out
+    #define istream INPUT
+    #define ostream OUTPUT
+};
+using namespace FastIO;
+#endif
+
+const int MAX = 100'000;
+const int treeSize = 4'000'000;
+
+template<typename T = int,
+        typename Func = plus<T>,
+        typename Inv_Func = minus<T>,
+        typename Update_Func = plus<T>>
+class PersistentSegTree {
+private:
+    Func f;
+    Inv_Func inv_f;
+    Update_Func u;
+
+    struct Node {
+        T val;
+        int l, r;
+
+        Node() : val(0), l(-1), r(-1) {}
+        Node(T val, int l, int r) : val(val), l(l), r(r) {}
+    } tree[treeSize];
+
+    int sz, ptr = 0;
+    std::vector<int> roots;
+    //std::vector<Node> tree;
+
+    void make_root() { // maybe not useful?
+        roots.emplace_back(ptr++);
+        //tree.emplace_back();
+    }
+
+    void _update(int prev, int curr, int s, int e, int idx, T val) {
+        tree[curr].val = u(tree[prev].val, val);
+
+        if (s == e) return;
+
+        int m = (s + e) >> 1;
+        if (idx <= m) {
+            tree[curr].l = ptr++;
+            //tree.emplace_back();
+            tree[curr].r = tree[prev].r;
+            _update(tree[prev].l, tree[curr].l, s, m, idx, val);
+        } else {
+            tree[curr].r = ptr++;
+            //tree.emplace_back();
+            tree[curr].l = tree[prev].l;
+            _update(tree[prev].r, tree[curr].r, m + 1, e, idx, val);
+        }
+    }
+
+    T _query(int node, int s, int e, int l, int r) {
+        if (e < l || r < s) return 0;
+        if (l <= s && e <= r) return tree[node].val;
+
+        int m = (s + e) >> 1;
+        return f(_query(tree[node].l, s, m, l, r), _query(tree[node].r, m + 1, e, l, r));
+    }
+
+    T _query_r(int node, int s, int e, int l) {
+        if (s == e) return tree[node].val;
+
+        int m = (s + e) >> 1;
+        if (l <= m) {
+            return _query_r(tree[node].l, s, m, l) + tree[tree[node].r].val;
+        } else {
+            return _query_r(tree[node].r, m + 1, e, l);
+        }
+    }
+
+public:
+    // Construct the empty tree with size of n
+    explicit PersistentSegTree(int n = MAX + 5) : sz(n) {
+        //tree.reserve(7'000'000); // n + q * log n
+
+        roots.emplace_back(ptr++); // Create root node
+        //tree.emplace_back();
+        tree[0].l = tree[0].r = 0;
+    }
+
+    void clear() {
+        roots.clear();
+        ptr = 0;
+
+        roots.emplace_back(ptr++); // Create root node
+        //tree.emplace_back();
+        tree[0].l = tree[0].r = 0;
+    }
+
+    int update(int idx, T val) {
+        int prev = roots.back();
+        roots.emplace_back(ptr++); // Create new root node
+        //tree.emplace_back();
+        int curr = roots.back();
+
+        _update(prev, curr, 0, sz - 1, idx, val);
+        return (int) roots.size() - 1;
+    }
+
+    int update(int version, int idx, T val) {
+        int prev = roots[version];
+        roots.emplace_back(ptr++); // Create new root node
+        //tree.emplace_back();
+        int curr = roots.back();
+
+        _update(prev, curr, 0, sz - 1, idx, val);
+        return (int) roots.size() - 1;
+    }
+
+    /**
+     * Returns the query over the range [l,r] when updates with index (i,j] are performed.
+     * @param i starting index of included updates
+     * @param j ending index of included updates
+     * @param l starting range of the query
+     * @param r ending range of the query
+     * @return querying result
+     */
+    T query(int i, int j, int l, int r) {
+        T res1 = _query(roots[i], 0, sz - 1, l, r);
+        T res2 = _query(roots[j], 0, sz - 1, l, r);
+        return inv_f(res2, res1);
+    }
+
+    T query(int i, int l, int r) {
+        return _query(roots[i], 0, sz - 1, l, r);
+    }
+
+    T query_r(int i, int l) {
+        return _query_r(roots[i], 0, sz - 1, l);
+    }
+};
+
+using pst = PersistentSegTree<>;
+pst PST;
+
+int32_t main() {
+    fastIO;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<pair<int,int>> q(n);
+        for (auto& [x, y] : q) cin >> x >> y;
+        sort(all(q));
+
+        vector<pair<int,int>> version;
+        version.reserve(q.size());
+        for (const auto& [x, y] : q) {
+            version.emplace_back(x, PST.update(y, 1));
+        }
+        version.emplace_back(MAX + 100, PST.update(MAX, 1)); // padding for upperbound
+
+        ll ans = 0;
+        while (m--) {
+            int x1, y1, x2, y2;
+            cin >> x1 >> x2 >> y1 >> y2;
+
+            auto it1 = lower_bound(all(version), make_pair(x1, INT_MIN));
+            auto it2 = upper_bound(all(version), make_pair(x2, INT_MAX));
+
+            ans += PST.query(it1->second - 1, it2->second - 1, y1, y2);
+        }
+        cout << ans << endl;
+        PST.clear();
+
+    }
+
+    return 0;
+}
+
+#ifdef ANOTHER_SOLUTION
+// Use only one FenwickTree(BIT) with some clever way of performing offline queries
+
+const int MAX = 100'000;
+
+template<typename T = int,
+         typename Func = plus<T>,
+         typename Inv_Func = minus<T>,
+         T Identity = 0>
+class FenWickTree {
+private:
+    Func f;
+    Inv_Func inv_f;
+
+    vector<T> tree;
+    int size;
+
+    void _update(int x, T val) {
+        for (int i = x; i < size; i += (i & -i)) {
+            tree[i] = f(tree[i], val);
+        }
+    }
+
+    T _query(int x) {
+        T res = Identity;
+        for (int i = x; i > 0; i -= (i & -i)) {
+            res = f(res, tree[i]);
+        }
+        return res;
+    }
+
+public:
+    // Empty tree constructor
+    FenWickTree(int size) : size(size) { // NOLINT
+        tree.resize(size, Identity);
+    }
+
+    FenWickTree(const vector<T> &v) {
+        size = (int) v.size();
+        tree.resize(size);
+        for (int i = 1; i < size; i++) {
+            _update(i, v[i]);
+        }
+    }
+
+    void update(int x, T val) {
+        _update(x, val);
+    }
+
+    T query(int left, int right) {
+        if (left > right) return Identity;
+        return inv_f(_query(right), _query(left - 1));
+    }
+}; // class FenWickTree
+
+int32_t main() {
+    fastIO;
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<int> q[MAX + 10];
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            q[x].emplace_back(y);
+        }
+
+        vector<tuple<int,int,int>> query[MAX + 10];
+        for (int i = 0; i < m; i++) {
+            int l, r, b, t;
+            cin >> l >> r >> b >> t;
+            query[l - 1].emplace_back(b, t, -1);
+            query[r].emplace_back(b, t, 1);
+        }
+
+        FenWickTree rsq(MAX + 10);
+        ll ans = 0;
+
+        for (int x = 0; x <= MAX; x++) {
+            for (const auto& y : q[x]) rsq.update(y, 1);
+            for (const auto& [b, t, sgn]: query[x]) {
+                ans += sgn * rsq.query(b, t);
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+#endif
