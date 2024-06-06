4b1b280f131ec710e17046305e3eb345ceb0f8d7
Aledo-Jun <june050190@gmail.com>
2024-06-06 17:45:24 +0900

    Add MarsSegTree

--- Utils/SegTree.hpp
+++ Utils/SegTree.hpp
@@ -535,4 +535,74 @@ namespace SegTree
         }
     }; // class FenWickTree_2D
 } // namespace SegTree
+
+namespace SpecialSegTree
+{
+/**
+ * @brief SegTree for Mars Map query i.e. counts the number of 0's
+ * @tparam T value type
+ */
+    template<typename T>
+    class MarsSegTree {
+    private:
+        vector<T> tree, tree2;
+        int size, height, n;
+
+        void init() {
+            for (int i = 0; i < n; i++) {
+                tree[i + size / 2] = 0;
+                tree2[i + size / 2] = 1;
+            }
+            for (int i = size / 2 - 1; i > 0; i--) {
+                tree2[i] = tree2[i << 1] + tree2[i << 1 | 1];
+            }
+        }
+
+        void apply(int node, T value) {
+            tree[node] += value;
+            if (tree[node] > 0) tree2[node] = 0;
+            else if (node >= size / 2) tree2[node] = 1;
+            else tree2[node] = tree2[node << 1] + tree2[node << 1 | 1];
+        }
+
+        void _update(int l, int r, T value) {
+            l += size / 2, r += size / 2;
+            int l0 = l, r0 = r;
+
+            for (; l <= r; l >>= 1, r >>= 1) {
+                if (l & 1) apply(l++, value);
+                if (~r & 1) apply(r--, value);
+            }
+
+            // pulling up
+            l = l0, r = r0;
+            while (l > 1) {
+                l >>= 1;
+                apply(l, 0);
+            }
+            while (r > 1) {
+                r >>= 1;
+                apply(r, 0);
+            }
+        }
+
+    public:
+        MarsSegTree(int n) : n(n) {
+            height = (int) ceil(log2(n));
+            size = (1 << (height + 1));
+            tree.resize(size + 1);
+            tree2.resize(size + 1);
+            init();
+        }
+
+        void update(int l, int r, T value) {
+            if (l > r) return;
+            _update(l, r, value);
+        }
+
+        int get_total_visible() {
+            return tree2[1];
+        }
+    }; // class MarsSegTree
+} // namespace SpecialSegTree
 } // namespace Utils
