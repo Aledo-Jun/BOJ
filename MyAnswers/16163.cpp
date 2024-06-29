9a0dfb0bfa236b0d8d4391fe4ee3ab747e3f2c82
Aledo-Jun <june050190@gmail.com>
2024-06-29 18:27:36 +0900

    solve 16163

---
+++ MyAnswers/16163.cpp
@@ -0,0 +1,311 @@
+//
+// Created by june0 on 2024-06-29.
+//
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3,unroll-loops")
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+using namespace std;
+using namespace __gnu_cxx;
+using ll [[maybe_unused]] = long long;
+using ull [[maybe_unused]] = unsigned long long;
+using lll [[maybe_unused]] = __int128;
+using lld [[maybe_unused]] = long double;
+using llld [[maybe_unused]] = __float128;
+#define GRAPH_TYPE_DEFINED
+template<typename T> using graph [[maybe_unused]] = vector<vector<pair<int,T>>>;
+#define MATRIX_TYPE_DEFINED
+template<typename T> using matrix [[maybe_unused]] = vector<vector<T>>;
+
+/**
+ *  "Premature optimization is the root of all evil."
+ *  <sub> Donald Knuth </sub>
+ */
+
+#ifdef ONLINE_JUDGE // Be careful if problem is about strings.
+/**
+ * Namespace for Fast I/O
+ *
+ * @class@b INPUT
+ * class which can replace the cin
+ *
+ * @class@b OUTPUT
+ * class which can replace the cout
+ *
+ * @Description
+ * These classes use low-level i/o functions (@c fread()/mmap() for input, @c fwrite()/write() for output)
+ * so that they can read or write much faster than cin and cout. <br>
+ * Several macros are defined for convenience of using them.
+ *
+ * @Author  <a href="https://blog.naver.com/jinhan814/222266396476">jinhan814</a>
+ * @Date    2021-05-06
+ */
+namespace FastIO {
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+    constexpr int SZ = 1 << 20;
+
+    /* Class INPUT */
+    class INPUT {
+    private:
+        char* p;
+        bool __END_FLAG__, __GET_LINE_FLAG__; // NOLINT
+    public:
+        explicit operator bool() const { return !__END_FLAG__; }
+        INPUT() {
+            struct stat st;
+            fstat(0, &st);
+            p = (char*)mmap(0, st.st_size, PROT_READ, MAP_SHARED, 0, 0);
+        }
+
+        static bool IsBlank(char c) { return c == ' ' || c == '\n'; }
+
+        static bool IsEnd(char c) { return c == '\0'; }
+
+        char _ReadChar() { // NOLINT
+            return *p++;
+        }
+
+        char ReadChar() {
+            char ret = _ReadChar();
+            for (; IsBlank(ret); ret = _ReadChar());
+
+            return ret;
+        }
+
+        template<class T>
+        T ReadInt() {
+            T ret = 0;
+            char curr = _ReadChar();
+            bool minus_flag = false;
+
+            for (; IsBlank(curr); curr = _ReadChar());
+            if (curr == '-') minus_flag = true, curr = _ReadChar();
+            for (; !IsBlank(curr) && !IsEnd(curr); curr = _ReadChar())
+                ret = 10 * ret + (curr & 15);
+            if (IsEnd(curr)) __END_FLAG__ = true;
+
+            return minus_flag ? -ret : ret;
+        }
+
+        std::string ReadString() {
+            std::string ret;
+            char curr = _ReadChar();
+            for (; IsBlank(curr); curr = _ReadChar());
+            for (; !IsBlank(curr) && !IsEnd(curr); curr = _ReadChar())
+                ret += curr;
+            if (IsEnd(curr)) __END_FLAG__ = true;
+
+            return ret;
+        }
+
+        double ReadDouble() {
+            std::string ret = ReadString();
+            return std::stod(ret);
+        }
+
+        std::string getline() {
+            std::string ret;
+            char curr = _ReadChar();
+            for (; curr != '\n' && !IsEnd(curr); curr = _ReadChar())
+                ret += curr;
+            if (__GET_LINE_FLAG__) __END_FLAG__ = true;
+            if (IsEnd(curr)) __GET_LINE_FLAG__ = true;
+
+            return ret;
+        }
+
+        friend INPUT &getline(INPUT &in, std::string &s) {
+            s = in.getline();
+            return in;
+        }
+    } _in;
+    /* End of Class INPUT */
+
+    /* Class OUTPUT */
+    class OUTPUT {
+    private:
+        char writeBuffer[SZ];
+        int write_idx;
+    public:
+        ~OUTPUT() { Flush(); }
+
+        explicit operator bool() const { return true; }
+
+        void Flush() {
+            write(1, writeBuffer, write_idx);
+            write_idx = 0;
+        }
+
+        void WriteChar(char c) {
+            if (write_idx == SZ) Flush();
+            writeBuffer[write_idx++] = c;
+        }
+
+        template<class T>
+        void WriteInt(T n) {
+            int sz = GetSize(n);
+            if (write_idx + sz >= SZ) Flush();
+            if (n < 0) writeBuffer[write_idx++] = '-', n = -n;
+            for (int i = sz; i-- > 0; n /= 10)
+                writeBuffer[write_idx + i] = n % 10 | 48;
+            write_idx += sz;
+        }
+
+        void WriteString(const std::string& s) {
+            for (auto &c: s) WriteChar(c);
+        }
+
+        void WriteDouble(double d) {
+            WriteString(std::to_string(d));
+        }
+
+        template<class T>
+        int GetSize(T n) {
+            int ret = 1;
+            for (n = n >= 0 ? n : -n; n >= 10; n /= 10) ret++;
+
+            return ret;
+        }
+    } _out;
+    /* End of Class OUTPUT */
+
+    /* Operators */
+    INPUT &operator>>(INPUT &in, char &i) {
+        i = in.ReadChar();
+        return in;
+    }
+
+    INPUT &operator>>(INPUT &in, std::string &i) {
+        i = in.ReadString();
+        return in;
+    }
+
+    template<class T, typename std::enable_if_t<std::is_arithmetic_v<T>> * = nullptr>
+    INPUT &operator>>(INPUT &in, T &i) {
+        if constexpr (std::is_floating_point_v<T>) i = in.ReadDouble();
+        else if constexpr (std::is_integral_v<T>) i = in.ReadInt<T>();
+        return in;
+    }
+
+    OUTPUT &operator<<(OUTPUT &out, char i) {
+        out.WriteChar(i);
+        return out;
+    }
+
+    OUTPUT &operator<<(OUTPUT &out, const std::string &i) {
+        out.WriteString(i);
+        return out;
+    }
+
+    template<class T, typename std::enable_if_t<std::is_arithmetic_v<T>> * = nullptr>
+    OUTPUT &operator<<(OUTPUT &out, T i) {
+        if constexpr (std::is_floating_point_v<T>) out.WriteDouble(i);
+        else if constexpr (std::is_integral_v<T>) out.WriteInt(i);
+        return out;
+    }
+
+    /* Macros for convenience */
+    #undef fastIO
+    #define fastIO 1
+    #define cin _in
+    #define cout _out
+    #define istream INPUT
+    #define ostream OUTPUT
+};
+using namespace FastIO;
+#endif
+
+template<typename String = std::string, int MAX_STRING_LENGTH = 2'000'000, int INDEX_SIZE = 26>
+class PalindromeTree {
+    struct node {
+        int nxt[INDEX_SIZE];
+        int sfxlink;
+        int len;
+        /* modify if needed */
+    };
+
+    String str;
+    int sfx; // max suffix palindrome
+    int total;
+    node tree[MAX_STRING_LENGTH + 5];
+
+    ll cnt[MAX_STRING_LENGTH + 5];
+
+    void append(int pos) {
+        int cur = sfx, cur_len;
+        int c = str[pos] - 'A';
+
+        while (true) {
+            cur_len = tree[cur].len;
+            if (pos - 1 - cur_len >= 0 && str[pos - 1 - cur_len] == str[pos]) break;
+            cur = tree[cur].sfxlink;
+        }
+        if (tree[cur].nxt[c]) {
+            sfx = tree[cur].nxt[c];
+            cnt[sfx]++;
+            return;
+        }
+
+        sfx = ++total;
+        tree[total].len = tree[cur].len + 2;
+        cnt[total] = 1;
+        tree[cur].nxt[c] = total;
+
+        if (tree[total].len == 1) {
+            tree[total].sfxlink = 2;
+            return;
+        }
+
+        while (true) {
+            cur = tree[cur].sfxlink;
+            cur_len = tree[cur].len;
+            if (pos - 1 - cur_len >= 0 && str[pos - 1 - cur_len] == str[pos]) {
+                tree[total].sfxlink = tree[cur].nxt[c];
+                break;
+            }
+        }
+    }
+
+public:
+    PalindromeTree() : sfx(2), total(2) {
+        tree[1].len = -1, tree[1].sfxlink = 1;
+        tree[2].len = 0, tree[2].sfxlink = 1;
+    }
+    void init(const String& s) {
+        str = s;
+        for (int i = 0; i < str.size(); i++) this->append(i);
+    }
+
+    [[nodiscard]]
+    std::size_t size() const { return total - 2; }
+
+    ll count() {
+        ll res = 0;
+        for (int i = total; i >= 3; i--) {
+            res += cnt[i];
+            cnt[tree[i].sfxlink] += cnt[i];
+        }
+        return res;
+    }
+
+}; // class PalindromeTree
+
+PalindromeTree<> ptree;
+
+int32_t main() {
+    fastIO;
+    string s;
+    cin >> s;
+    ptree.init(s);
+
+    cout << ptree.count();
+
+    return 0;
+}
