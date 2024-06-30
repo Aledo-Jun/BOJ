d44bfb6bd70d8f43a9589cff513286076084d3e6
Aledo-Jun <june050190@gmail.com>
2024-06-30 23:36:18 +0900

    solve 16877

---
+++ MyAnswers/16877.cpp
@@ -0,0 +1,265 @@
+//
+// Created by june0 on 2024-06-30.
+//
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+
+/**
+ *  "Premature optimization is the root of all evil."
+ *  <sub> Donald Knuth </sub>
+ */
+
+#ifdef ONLINE_JUDGE // Be careful if problem is about strings.
+/**
+ * Namespace for Fast I/O
+ *
+ * @class@b INPUT
+ * class which can replace the cin
+ *
+ * @class@b OUTPUT
+ * class which can replace the cout
+ *
+ * @Description
+ * These classes use low-level i/o functions (@c fread()/mmap() for input, @c fwrite()/write() for output)
+ * so that they can read or write much faster than cin and cout. <br>
+ * Several macros are defined for convenience of using them.
+ *
+ * @Author  <a href="https://blog.naver.com/jinhan814/222266396476">jinhan814</a>
+ * @Date    2021-05-06
+ */
+namespace FastIO {
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+    constexpr int SZ = 1 << 20;
+
+    /* Class INPUT */
+    class INPUT {
+    private:
+        char* p;
+        bool __END_FLAG__, __GET_LINE_FLAG__; // NOLINT
+    public:
+        explicit operator bool() const { return !__END_FLAG__; }
+        INPUT() {
+            struct stat st;
+            fstat(0, &st);
+            p = (char*)mmap(0, st.st_size, PROT_READ, MAP_SHARED, 0, 0);
+        }
+
+        static bool IsBlank(char c) { return c == ' ' || c == '\n'; }
+
+        static bool IsEnd(char c) { return c == '\0'; }
+
+        char _ReadChar() { // NOLINT
+            return *p++;
+        }
+
+        char ReadChar() {
+            char ret = _ReadChar();
+            for (; IsBlank(ret); ret = _ReadChar());
+
+            return ret;
+        }
+
+        template<class T>
+        T ReadInt() {
+            T ret = 0;
+            char curr = _ReadChar();
+            bool minus_flag = false;
+
+            for (; IsBlank(curr); curr = _ReadChar());
+            if (curr == '-') minus_flag = true, curr = _ReadChar();
+            for (; !IsBlank(curr) && !IsEnd(curr); curr = _ReadChar())
+                ret = 10 * ret + (curr & 15);
+            if (IsEnd(curr)) __END_FLAG__ = true;
+
+            return minus_flag ? -ret : ret;
+        }
+
+        std::string ReadString() {
+            std::string ret;
+            char curr = _ReadChar();
+            for (; IsBlank(curr); curr = _ReadChar());
+            for (; !IsBlank(curr) && !IsEnd(curr); curr = _ReadChar())
+                ret += curr;
+            if (IsEnd(curr)) __END_FLAG__ = true;
+
+            return ret;
+        }
+
+        double ReadDouble() {
+            std::string ret = ReadString();
+            return std::stod(ret);
+        }
+
+        std::string getline() {
+            std::string ret;
+            char curr = _ReadChar();
+            for (; curr != '\n' && !IsEnd(curr); curr = _ReadChar())
+                ret += curr;
+            if (__GET_LINE_FLAG__) __END_FLAG__ = true;
+            if (IsEnd(curr)) __GET_LINE_FLAG__ = true;
+
+            return ret;
+        }
+
+        friend INPUT &getline(INPUT &in, std::string &s) {
+            s = in.getline();
+            return in;
+        }
+    } _in;
+    /* End of Class INPUT */
+
+    /* Class OUTPUT */
+    class OUTPUT {
+    private:
+        char writeBuffer[SZ];
+        int write_idx;
+    public:
+        ~OUTPUT() { Flush(); }
+
+        explicit operator bool() const { return true; }
+
+        void Flush() {
+            write(1, writeBuffer, write_idx);
+            write_idx = 0;
+        }
+
+        void WriteChar(char c) {
+            if (write_idx == SZ) Flush();
+            writeBuffer[write_idx++] = c;
+        }
+
+        template<class T>
+        void WriteInt(T n) {
+            int sz = GetSize(n);
+            if (write_idx + sz >= SZ) Flush();
+            if (n < 0) writeBuffer[write_idx++] = '-', n = -n;
+            for (int i = sz; i-- > 0; n /= 10)
+                writeBuffer[write_idx + i] = n % 10 | 48;
+            write_idx += sz;
+        }
+
+        void WriteString(const std::string& s) {
+            for (auto &c: s) WriteChar(c);
+        }
+
+        void WriteDouble(double d) {
+            WriteString(std::to_string(d));
+        }
+
+        template<class T>
+        int GetSize(T n) {
+            int ret = 1;
+            for (n = n >= 0 ? n : -n; n >= 10; n /= 10) ret++;
+
+            return ret;
+        }
+    } _out;
+    /* End of Class OUTPUT */
+
+    /* Operators */
+    INPUT &operator>>(INPUT &in, char &i) {
+        i = in.ReadChar();
+        return in;
+    }
+
+    INPUT &operator>>(INPUT &in, std::string &i) {
+        i = in.ReadString();
+        return in;
+    }
+
+    template<class T, typename std::enable_if_t<std::is_arithmetic_v<T>> * = nullptr>
+    INPUT &operator>>(INPUT &in, T &i) {
+        if constexpr (std::is_floating_point_v<T>) i = in.ReadDouble();
+        else if constexpr (std::is_integral_v<T>) i = in.ReadInt<T>();
+        return in;
+    }
+
+    OUTPUT &operator<<(OUTPUT &out, char i) {
+        out.WriteChar(i);
+        return out;
+    }
+
+    OUTPUT &operator<<(OUTPUT &out, const std::string &i) {
+        out.WriteString(i);
+        return out;
+    }
+
+    template<class T, typename std::enable_if_t<std::is_arithmetic_v<T>> * = nullptr>
+    OUTPUT &operator<<(OUTPUT &out, T i) {
+        if constexpr (std::is_floating_point_v<T>) out.WriteDouble(i);
+        else if constexpr (std::is_integral_v<T>) out.WriteInt(i);
+        return out;
+    }
+
+    /* Macros for convenience */
+    #undef fastIO
+    #define fastIO 1
+    #define cin _in
+    #define cout _out
+    #define istream INPUT
+    #define ostream OUTPUT
+};
+using namespace FastIO;
+#endif
+
+vector<int> fib;
+
+class SpragueGrundy {
+    int n;
+    std::vector<int> grundyNumbers;
+
+public:
+    explicit SpragueGrundy(int size) : n(size), grundyNumbers(size + 1, 0) {}
+
+    void calc_set() {
+        for (int i = 1; i <= n; i++) {
+            set<int> s;
+            for (const auto &f : fib) {
+                if (f > i) break;
+                s.emplace(grundyNumbers[f] ^ grundyNumbers[i - f]);
+            }
+            while (s.count(grundyNumbers[i])) grundyNumbers[i]++;
+        }
+    }
+
+    void calc_bit() {
+        for (int i = 1; i <= n; i++) {
+            unsigned bit = 0;
+            for (const auto &f: fib) {
+                if (f > i) break;
+                bit |= 1U << grundyNumbers[i - f];
+            }
+            grundyNumbers[i] = __builtin_ctz(~bit);
+        }
+    }
+
+    int operator[](int i) { return grundyNumbers[i]; }
+
+};
+
+int32_t main() {
+    fastIO;
+    fib = {1, 1};
+    for (int i = 2; fib[i - 1] + fib[i - 2] < 3'000'000; i++) fib.emplace_back(fib[i - 1] + fib[i - 2]);
+
+    int n;
+    cin >> n;
+
+    SpragueGrundy solver(3'000'000);
+    solver.calc_bit();
+
+    int ans = 0, e;
+    while (n--) {
+        cin >> e;
+        ans ^= solver[e];
+    }
+
+    cout << (ans ? "koosaga" : "cubelover");
+
+    return 0;
+}
