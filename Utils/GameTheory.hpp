5804aacfb29ab461165b88a6f7085f9921ba9b85
Aledo-Jun <june050190@gmail.com>
2024-07-10 22:25:15 +0900

    Add SpragueGrundy2D

--- Utils/GameTheory.hpp
+++ Utils/GameTheory.hpp
@@ -5,6 +5,7 @@
 #ifndef BOJ_GAMETHEORY_HPP
 #define BOJ_GAMETHEORY_HPP
 
+#include <cstring>
 #include <vector>
 #include <set>
 
@@ -31,6 +32,44 @@ namespace Utils
             int operator[](int i) { return grundyNumbers[i]; }
 
         };
+
+        class SpragueGrundy2D {
+            int r, c;
+            std::vector<std::vector<int>> grundyNumbers;
+
+        public:
+            explicit SpragueGrundy2D(int r, int c) : r(r), c(c), grundyNumbers(r + 1, std::vector<int>(c + 1, 0)) {
+                calc_set();
+            }
+
+            void calc_set() {
+                //ll mx = 0;
+                for (int i = 1; i <= r; i++) {
+                    int g = 0;
+                    int cnt[4040];
+                    std::memset(cnt, 0, sizeof(cnt));
+                    for (int j = 1; j <= c; j++) {
+
+                        for (int k = 1; k < i; k++) cnt[grundyNumbers[i-k][j]]++;
+                        for (int k = 1; k < std::min(i, j); k++) cnt[grundyNumbers[i-k][j-k]]++;
+
+                        g = std::max(0, g - 100);
+                        while (cnt[g]) g++;
+                        grundyNumbers[i][j] = g;
+                        cnt[g] = 1;
+
+                        for (int k = 1; k < i; k++) cnt[grundyNumbers[i-k][j]]--;
+                        for (int k = 1; k < std::min(i, j); k++) cnt[grundyNumbers[i-k][j-k]]--;
+                    }
+                }
+                //cout << mx << endl << endl; // => 4012
+            }
+
+            int get(int i, int j) {
+
+            }
+        };
+
 
     }
 }
