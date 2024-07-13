84d32273513c74ff0b1300db3fef6b21a71827f0
Aledo-Jun <june050190@gmail.com>
2024-07-14 01:00:06 +0900

    Add some Quality-of-Life templates

---
+++ Utils/using_templates.hpp
@@ -0,0 +1,27 @@
+//
+// Created by june0 on 2024-07-14.
+//
+
+#ifndef BOJ_USING_TEMPLATES_HPP
+#define BOJ_USING_TEMPLATES_HPP
+
+#include <bits/stdc++.h>
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3,unroll-loops")
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl '\n'
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+using namespace std;
+using ll [[maybe_unused]] = long long;
+using ull [[maybe_unused]] = unsigned long long;
+using lll [[maybe_unused]] = __int128;
+using lld [[maybe_unused]] = long double;
+using llld [[maybe_unused]] = __float128;
+#define GRAPH_TYPE_DEFINED
+template<typename T> using graph [[maybe_unused]] = vector<vector<pair<int,T>>>;
+#define MATRIX_TYPE_DEFINED
+template<typename T> using matrix [[maybe_unused]] = vector<vector<T>>;
+
+
+#endif //BOJ_USING_TEMPLATES_HPP
